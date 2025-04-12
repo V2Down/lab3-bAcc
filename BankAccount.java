568873824a2a7792e927b41caf96b7b4d9805095
V2Down <aceball2@depaul.edu>
2025-04-12 14:37:37 -0500

    BankAccount Updated
    Can't withdraw with balance of zero, and one percent interest added to deposit amount

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,13 @@
+//------------------------------------------------------
+// Lab 4
+// Written by: Alexis Ortega-Ceballos 1948124
+// For SES350 Section 601 – Spring 2025
+//--------------------------------------------------------
+
+/*
+ In this code we added the inability to withdraw if the balance is zero
+ also we added a 1% interest extra to the deposit amount
+*/
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +25,15 @@ public class BankAccount {
   }
   
   public void withdraw(double amount) {
+    if (balance == 0) {
+      return;
+    }
     balance -= amount;
     transactionHistory.add("Withdrawal: -" + amount);
   }
   
   public void deposit(double amount) {
+    amount += amount * 0.01;
     balance += amount;
     transactionHistory.add("Deposit: +" + amount);
   }
